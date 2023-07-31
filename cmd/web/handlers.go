e8fcf82a60870f34ab436528c332a8e22f0d14d6
kipkemei <sangdennis9@gmail.com>
2023-07-31 08:50:23 +0300

    ADD: html templating and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +14,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	// Initialize a slice containing the paths to the two files.
+	// The base template file must be the first in the slice.
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
+	}
+
+	// Use template.ParseFiles() to read the template file into a
+	// template set. Pass the slice of files paths as a variadic parameter
+	// If an error occurs, log a detailed error msg and use  http.Error()
+	// to send a generic 500 Internal Server Error response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Use ExecuteTemplate() method on the template set to write the template
+	// content as the response body. The last parameter to ExecuteTemplate()
+	// represents any dynamic data that we want to pass in, nil for now.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
