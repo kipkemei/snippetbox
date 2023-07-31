b27e828945b4379ba588f987216b5e348c426962
kipkemei <sangdennis9@gmail.com>
2023-07-31 11:56:50 +0300

    FIX: manage cofiguration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls. The value of the flag 
+	// will be stored in the 'addr' variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Use flag.Parse() to read the command-line flag value and assign it to 'addr'.
+	// It should be called before the variable is used, else it'll contain the default value.
+	// The application is terminated if any errors are encountered during parsing.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of "./ui/static" directory.
@@ -24,8 +35,8 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
 
@@ -43,7 +54,7 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	if err != nil {
         return nil, err
     }
-	
+
     if s.IsDir() {
         index := filepath.Join(path, "index.html")
         if _, err := nfs.fs.Open(index); err != nil {
