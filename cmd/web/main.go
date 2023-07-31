f16c5b9bbbe57713e0e191f7a53e09eddbdc89c0
kipkemei <sangdennis9@gmail.com>
2023-07-31 10:00:21 +0300

    ADD: serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of "./ui/static" directory.
+	// The path given to http.Dir() is relative to the project root.
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+
+	// Use mux.Handle() to register the file server as the handler for all URL
+	// paths that start with "/static/". For matching parts, strip the prefix
+	// "/static" before the request reaches the file server.
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other application routes as normal.
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
@@ -15,3 +28,33 @@ func main() {
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+    f, err := nfs.fs.Open(path)
+    if err != nil {
+        return nil, err
+    }
+
+    s, err := f.Stat()
+	if err != nil {
+        return nil, err
+    }
+	
+    if s.IsDir() {
+        index := filepath.Join(path, "index.html")
+        if _, err := nfs.fs.Open(index); err != nil {
+            closeErr := f.Close()
+            if closeErr != nil {
+                return nil, closeErr
+            }
+
+            return nil, err
+        }
+    }
+
+    return f, nil
+}    
