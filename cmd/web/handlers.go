cca9f5fc29806cf1cdad30e83c10135a3f3b5300
kipkemei <sangdennis9@gmail.com>
2023-08-02 09:49:27 +0300

    ADD: create template actions and functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
@@ -23,35 +22,36 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	// Initialize a slice containing the paths to the two files.
+	// The base template file must be the first in the slice.
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
 	}
 
-	// // Initialize a slice containing the paths to the two files.
-	// // The base template file must be the first in the slice.
-	// files := []string{
-	// 	"./ui/html/base.html",
-	// 	"./ui/html/partials/nav.html",
-	// 	"./ui/html/pages/home.html",
-	// }
+	// Use template.ParseFiles() to read the template file into a
+	// template set. Pass the slice of files paths as a variadic parameter
+	// If an error occurs, log a detailed error msg and use  http.Error()
+	// to send a generic 500 Internal Server Error response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Use the serverError() helper
+		return
+	}
 
-	// // Use template.ParseFiles() to read the template file into a
-	// // template set. Pass the slice of files paths as a variadic parameter
-	// // If an error occurs, log a detailed error msg and use  http.Error()
-	// // to send a generic 500 Internal Server Error response to the user.
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err) // Use the serverError() helper
-	// 	return
-	// }
+	// Create an instance of a templateData struct holding the slice of snippets.
+	data := &templateData{
+		Snippets: snippets,
+	}
 
-	// // Use ExecuteTemplate() method on the template set to write the template
-	// // content as the response body. The last parameter to ExecuteTemplate()
-	// // represents any dynamic data that we want to pass in, nil for now.
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, err) // Use the serverError() helper
-	// }
+	// Use ExecuteTemplate() method on the template set to write the template
+	// content as the response body. The last parameter to ExecuteTemplate()
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err) // Use the serverError() helper
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -8,5 +8,6 @@ import "snippetbox.sangdennis.com/internal/models"
 // to it as the build progresses.
 type templateData struct {
 	Snippet *models.Snippet
+	Snippets []*models.Snippet
 }
 
