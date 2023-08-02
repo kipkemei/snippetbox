02338182b4f12dbe2d82dfb62267cc89c35a9945
kipkemei <sangdennis9@gmail.com>
2023-08-02 12:16:22 +0300

    ADD: add a custom template function humanDate() to transform dates.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"snippetbox.sangdennis.com/internal/models"
 )
@@ -15,6 +16,20 @@ type templateData struct {
 	CurrentYear int
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+// Create humanDate() which returns a nicely formatted string representation 
+// of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+} 
+
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custome template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -36,12 +51,15 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Parse the base template into a template set
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		// The template.FuncMap must be registered with the template set before
+		// the ParseFiles() method is called. Use template.New() to create an
+		// empty template set, use Funcs() method to register the template.FuncMap,
+		//  and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
-
+		
 		// Call ParseGlob() *on this template set* to add any partials.
 		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
 		if err != nil {
